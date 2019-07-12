468e32b2bcb2a3d7eee54a18a01b06f55b321427
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2019-07-12 16:47:50 +0200

    added further code for Login.Vue and sample code into Login(Materialise with Mat-CSS

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,154 @@
+<!--Vuetify material: Dialogs/Form -->
 <template>
-  <div>
-    <h1 class="pa-2">LOGIN VIEW</h1>
-  </div>
+  <v-layout row justify-center>
+    <!-- template for the SIGNUP -->
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">Signup</v-btn>
+
+        <!-- <v-btn color="primary" dark v-on="on">Logout</v-btn> -->
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Signup Form</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
+              </v-flex>
+              <v-flex xs12 sm6></v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Submit</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- template for the LOGIN -->
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">Login</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Login Form</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <!-- <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>-->
+              <v-flex xs12>
+                <v-text-field label="Email*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-flex>
+              <!-- <v-flex xs12 sm6>
+                <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
+              </!-->
+              -->
+              <v-flex xs12 sm6></v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <!-- <v-btn color="blue darken-1" flat @click="dialog = false">Cancel</v-btn> -->
+          <v-btn color="blue darken-1" flat @click="dialog = false">LOGIN</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- template for the LOGOUT -->
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">Logout</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Logout Form</span>
+        </v-card-title>
+        <!-- <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
+              </v-flex>
+              <v-flex xs12 sm6></v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>-->
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Submit</v-btn>
+        </v-card-actions>
+      </v-card>
+      <!-- no form required -->
+    </v-dialog>
+  </v-layout>
 </template>
 
+
 <script>
+export default {
+  data: () => ({
+    dialog: false
+  })
+};
 </script>
 
 
